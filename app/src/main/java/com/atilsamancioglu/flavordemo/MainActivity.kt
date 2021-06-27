80861e7bef729af40e0b20062f0b2496c3058dd1
atilsamancioglu <samancioglu.atil@gmail.com>
2021-06-27 22:00:31 +0300

    Initial commit

---
+++ app/src/main/java/com/atilsamancioglu/flavordemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.atilsamancioglu.flavordemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        callRetrofit(BuildConfig.BASE_URL)
+
+    }
+
+    fun callRetrofit(url : String) {
+        //demo call
+        println(url)
+    }
+}
